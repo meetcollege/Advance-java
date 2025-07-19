2cfdc2c2fe49bd760bc1cbd24801a78be2027746
meetcollege <meetn0101@gmail.com>
2025-07-19 11:03:25 +0530

    crud complete

--- crud/src/crud/Crud.java
+++ crud/src/crud/Crud.java
@@ -1,74 +1,115 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package crud;
 
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
-/**
- *
- * @author Administrator
- */
 public class Crud {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        // connection credentials
+        // Connection credentials
         String url = "jdbc:mysql://localhost:3309/firstp";
         String user = "root";
         String password = "";
 
         try {
-            // driver loading
+            // Driver loading
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // database connection
+            // Database connection
             Connection conn = DriverManager.getConnection(url, user, password);
             Statement st = conn.createStatement();
             System.out.println("Database is connected");
 
-            // switch case for different operation
-            System.out.println("1. Insert\n2. Update\n3. Delete\n4. Retrive");
-            System.out.print("Enter choice : ");
-            int ch = sc.nextInt();
+            // Switch case for different operations
+            while (true) {
+                System.out.println("\n1. Insert\n2. Update\n3. Delete\n4. Retrieve\n5. Exit");
+                System.out.print("Enter choice: ");
+                int ch = sc.nextInt();
 
-            String query = null;
-            int id = 0;
-            String name = null;
-            String department = null;
-            switch (ch) {
-                case 1:
-                    System.out.print("Enter emp id : ");
-                    id = sc.nextInt();
-                    System.out.print("Enter emp name : ");
-                    name = sc.next();
-                    System.out.print("ENter emp department : ");
-                    department = sc.next();
-                    query = "insert into emp (id,name,dept) values (" + id + ",'" + name + "','" + department + "')";
-                    int rowsAffected = st.executeUpdate(query);
-                    if (rowsAffected > 0) {
-                        System.out.println("Employee inserted successfully");
-                    }
-                    break;
-                case 2:
-                    
-                 
+                String query = null;
+                int id = 0;
+                String name = null;
+                String department = null;
+
+                switch (ch) {
+                    case 1:
+                        // Insert Operation
+                        System.out.print("Enter emp id: ");
+                        id = sc.nextInt();
+                        System.out.print("Enter emp name: ");
+                        name = sc.next();
+                        System.out.print("Enter emp department: ");
+                        department = sc.next();
+
+                        query = "INSERT INTO emp (id, name, dept) VALUES (" + id + ", '" + name + "', '" + department + "')";
+                        int rowsInserted = st.executeUpdate(query);
+                        if (rowsInserted > 0) {
+                            System.out.println("Employee inserted successfully.");
+                        }
+                        break;
+
+                    case 2:
+                        // Update Operation
+                        System.out.print("Enter emp id to update: ");
+                        id = sc.nextInt();
+                        System.out.print("Enter new emp name: ");
+                        name = sc.next();
+                        System.out.print("Enter new emp department: ");
+                        department = sc.next();
+
+                        query = "UPDATE emp SET name = '" + name + "', dept = '" + department + "' WHERE id = " + id;
+                        int rowsUpdated = st.executeUpdate(query);
+                        if (rowsUpdated > 0) {
+                            System.out.println("Employee updated successfully.");
+                        } else {
+                            System.out.println("Employee with id " + id + " not found.");
+                        }
+                        break;
+
+                    case 3:
+                        // Delete Operation
+                        System.out.print("Enter emp id to delete: ");
+                        id = sc.nextInt();
+
+                        query = "DELETE FROM emp WHERE id = " + id;
+                        int rowsDeleted = st.executeUpdate(query);
+                        if (rowsDeleted > 0) {
+                            System.out.println("Employee deleted successfully.");
+                        } else {
+                            System.out.println("Employee with id " + id + " not found.");
+                        }
+                        break;
+
+                    case 4:
+                        // Retrieve Operation
+                        System.out.println("Employee List:");
+                        query = "SELECT * FROM emp";
+                        ResultSet rs = st.executeQuery(query);
+                        while (rs.next()) {
+                            System.out.println("ID: " + rs.getInt("id") + ", Name: " + rs.getString("name") + ", Department: " + rs.getString("dept"));
+                        }
+                        break;
+
+                    case 5:
+                        System.out.println("Exiting program.");
+                        conn.close();
+                        System.exit(0);
+                        break;
+
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
             }
 
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
-
 }
